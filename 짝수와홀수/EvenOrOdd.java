ed1a76c3099b52da4cd254c8d6b046d23973241a
ParkTaegyu <01046009100@hanmail.net>
2017-08-29 17:17:32 +0900

    짝수와 홀수

---
+++ 짝수와홀수/EvenOrOdd.java
@@ -0,0 +1,18 @@
+package 짝수와홀수;
+
+/**
+ * 짝수와 홀수 - 2017/08/29 - Park Taegyu
+ * */
+public class EvenOrOdd {
+    String evenOrOdd(int num) {
+        return (num%2==0) ? "Even" : "Odd";
+    }
+
+    public static void main(String[] args) {
+        String str = "1 2 3 4";
+        EvenOrOdd evenOrOdd = new EvenOrOdd();
+        //아래는 테스트로 출력해 보기 위한 코드입니다.
+        System.out.println("결과 : " + evenOrOdd.evenOrOdd(3));
+        System.out.println("결과 : " + evenOrOdd.evenOrOdd(2));
+    }
+}
